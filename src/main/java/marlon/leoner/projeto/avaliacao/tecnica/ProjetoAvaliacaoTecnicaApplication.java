9f2c5faa558d9ffd4e001210cde21d50f3a95a9d
Marlon Leoner <marlon.leoner@gmail.com>
2023-11-10 20:34:17 -0300

    add: função de scheduler adicionado ao projeto

--- src/main/java/marlon/leoner/projeto/avaliacao/tecnica/ProjetoAvaliacaoTecnicaApplication.java
+++ src/main/java/marlon/leoner/projeto/avaliacao/tecnica/ProjetoAvaliacaoTecnicaApplication.java
@@ -2,8 +2,10 @@ package marlon.leoner.projeto.avaliacao.tecnica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProjetoAvaliacaoTecnicaApplication {
 
 	public static void main(String[] args) {
